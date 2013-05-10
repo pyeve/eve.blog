c5fd5fd00860f40ca746c6f179c22d876b6f362f
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-05-10 23:52:42 +0200

    First commit

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Nicola Iarocci'
+SITENAME = u'The Eve Blog'
+SITEURL = 'blog.python-eve.org'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Eve Website', 'http://python-eve.org'),
+          ('GitHub', 'http://github.com/nicolaiarocci/eve'),
+          ('IRC', 'irc://irc.freenode.net/evehq'),
+          ('PyPI', 'http://pypi.python.org/pypi/Eve'),
+          ('Support', 'http://python-eve.org/support.html'),
+          ('Contributing', 'http://python-eve.org/contributing.html'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/nicolaiarocci'),
+          ('github', 'https://github.com/nicolaiarocci/eve'),)
+
+THEME = 'themes/iris.eve'
+
+EMAIL = 'eve@nicolaiarocci.com'
+
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
---
+++ publishconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+#SITEURL = 'http://blog.python-eve.org/'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
+ARTICLE_URL = '{slug}'
+PAGE_URL = 'pages/{slug}'
+AUTHOR_URL = 'author/{slug}'
+CATEGORY_URL = 'category/{slug}'
+TAG_URL = 'tag/{slug}'
